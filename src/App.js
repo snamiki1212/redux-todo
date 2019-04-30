37abb5354eb9a01a6e37d0a6b2a0ed2792750384
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 09:30:02 +0700

    Add Todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div className="App">
       test
+      <Todo text="this is text" />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Todo = ({ text }) => {
+  return (
+    <li>{text}</li>
+  )
+}
+
+export default Todo;
