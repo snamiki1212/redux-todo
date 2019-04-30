47ee71fb064c1a410a16729807f910695f3f38f2
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 14:52:14 +0700

    Updates

---
+++ precache-manifest.7d298216f57ec17e734357e756637b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5938f320d16dae2bbc247782f33ed711",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "b31a6e87f271b3b33dab",
+    "url": "/redux-todo/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "0561df2f1c7360f8b6ba",
+    "url": "/redux-todo/static/js/2.f49b2791.chunk.js"
+  },
+  {
+    "revision": "b31a6e87f271b3b33dab",
+    "url": "/redux-todo/static/js/main.9342a356.chunk.js"
+  },
+  {
+    "revision": "4a2d5658e1b8a4be0a5f",
+    "url": "/redux-todo/static/js/runtime~main.134d15df.js"
+  }
+]);
