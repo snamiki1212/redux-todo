ab4f753ce6f8101d8a3cb9b472907eead94fddd9
snamiki1212 <snamiki1212@gmail.com>
2019-04-30 11:09:08 +0700

    Refactor

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,26 +1,22 @@
-const todos = (state = [
+const initialTodos = [
   {id: 1, text: 'this is text a', completed: false},
   {id: 2, text: 'this is text b', completed: false},
   {id: 3, text: 'this is text c', completed: false}
-], action) => {
+];
+
+const todos = (state = initialTodos, action) => {
   switch(action.type){
     case 'TOGGLE_TODO':
       console.log('REDUCER: here is toggle-todo reducer');
       
-      let o;
-      console.dir(state);
-      console.dir(action);
       return state.map(ele => {
         if(ele.id === action.id){
-          o = {...ele, completed: !ele.completed}
-          console.log(o);
-          return o;
+          return {...ele, completed: !ele.completed};
         }
         return ele
       });
     default:
       console.log('REDUCER: here is default reducer');
-      console.dir(action);
       return state;
   }
 }
